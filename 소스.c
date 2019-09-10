75336f0e5b305f2d662b41e24d67458fce96968c
20181125 <oo_303@naver.com>
2019-09-10 12:46:08 +0900

    quiz 2nd commit

--- 소스.c
+++ 소스.c
@@ -12,9 +12,14 @@ int main(void) {
 	for (int i = 0; i < 10; i++) {
 		a = rand() % 10 + 1;
 		p[i] = a;
+
+		for (int j = 0; j < i; j++) {
+			if (p[j] == a)
+				i--;
+		}
 	}
 
 	for (int i = 0; i < 10; i++) {
-		printf("%d ",p[i]);
+		printf("%d ", p[i]);
 	}
 }
