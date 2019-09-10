ee224221af3cc8dfbb025b364a048fec185f7ae6
20181125 <oo_303@naver.com>
2019-09-11 00:59:19 +0900

    나) 중복되지 않는 10개의 숫자 랜덤 반환

--- random.java
+++ random.java
@@ -5,10 +5,17 @@ import java.util.Random;
 public class random {
 	public static void main(String[] args) {
 		int[] p = new int[10];
+		int a;
 		
 		for(int i=0;i<10;i++) {
-			p[i]=new Random().nextInt(10)+1;
-			System.out.print(p[i] + " ");
+			p[i]=a=new Random().nextInt(10)+1;
+			for(int j=0;j<i;j++) {
+				if(p[j]==a)
+					i--;
+			}
 		}
+		
+		for(int i=0;i<10;i++)
+			System.out.print(p[i] + " ");
 	}
 }
